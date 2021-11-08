353e705eaca4ed2864ef7d13fd884319f5b6e197
Ruber <ruberfc@gmail.com>
2021-11-07 21:36:32 -0500

    Guardando cambios

---
+++ client/build/precache-manifest.4fbefa6fed84bddc2f39909b6b259c38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb04fe960d5a775cfa3b5406bb9cad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f75b8e94bc8f7abe0036",
+    "url": "/static/css/2.e603e8eb.chunk.css"
+  },
+  {
+    "revision": "f75b8e94bc8f7abe0036",
+    "url": "/static/js/2.d1f53755.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d1f53755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffea186b35bb5524ff8",
+    "url": "/static/js/main.1703c325.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
